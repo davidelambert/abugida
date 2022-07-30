2cbd0c0bfdc355de5f81b96e058072c900626f36
David E. Lambert <david@davidelambert.com>
2022-07-30 16:02:17 -0400

    initial vogon revision with text-to-speech

--- abugida_2.py
+++ abugida_2.py
@@ -1,59 +1,78 @@
 import sys
 import random
+import uuid
 from pathlib import Path
+
+import gtts
+from pedalboard import (Pedalboard,
+                        Distortion,
+                        Chorus,
+                        Delay,
+                        PitchShift,
+                        Limiter)
+from pedalboard.io import AudioFile
+from pydub import AudioSegment
+from playsound import playsound
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 
 
 HERE = Path(__file__).parent.resolve()
+TMP = HERE/'tmp/'
+if not TMP.exists():
+    TMP.mkdir()
 
 VOWELS = ('A', 'U', 'I', 'Ai', 'Au')
 CONSONANTS = ('B', 'G', 'D')
 SYLLABLES = [c + v.lower() for c in CONSONANTS for v in VOWELS] + list(VOWELS)
-
 MAX_SYL = 4
 
-
-def word(n_syl: None | int = None) -> str:
-    if n_syl is None:
-        n_syl = random.randint(1, MAX_SYL)
-
-    if n_syl == 0:
-        n_syl = random.randint(1, MAX_SYL)
-
-    if n_syl < 0:
-        n_syl = abs(n_syl)
-
-    if n_syl > MAX_SYL:
-        n_syl = n_syl % MAX_SYL
-
-    text = ''
-    i = 0
-    while i < n_syl - 1:  # all but last syllable
-        text = text + random.choice(SYLLABLES) + '-'
-        i += 1
-    text = text + random.choice(SYLLABLES)  # no hyphen after last syllable
-
-    return text
+LANGS = list(gtts.lang.tts_langs())
+ACCENT = {
+    'en': ['com.au', 'co.uk', 'com', 'ca', 'co.in', 'ie', 'co.za'],
+    'fr': ['ca', 'fr'],
+    'pt': ['com.br', 'pt'],
+    'es': ['com', 'com.mx', 'es'],
+}
 
 
-def line() -> str:
-    max_length = 30
-    n_words = random.randint(3, 5)
-    text = ''
+class Line:
+    def __init__(self):
+        self.display = ''
 
-    i = 0
-    while i < n_words:
-        text += word()
-        if len(text) < max_length:
-            text += ' '
+        max_len = 30
+        n_words = random.randint(3, 5)
+
+        i = 0
+        while i < n_words - 1:  # spaces after all but last word
+            self.display += self.word()
+            self.display += ' '
+            i += 1
+        self.display += self.word()
         i += 1
 
-    while len(text) > max_length:
-        text = text[:text.rfind('-')]
+        while len(self.display) > max_len:
+            self.display = self.display[:self.display.rfind('-')]
 
-    return text
+        self.text = self.display.replace('-', '').capitalize()
+
+    @classmethod
+    def word(cls):
+        n_syl = random.randint(1, MAX_SYL)
+        text = ''
+        i = 0
+        while i < n_syl - 1:  # all but last syllable
+            text = text + random.choice(SYLLABLES) + '-'
+            i += 1
+        text = text + random.choice(SYLLABLES)  # no hyphen after last syllable
+        return text
+
+    def __repr__(self):
+        return self.display
+
+    def __str__(self):
+        return self.text
 
 
 class MainWindow(QMainWindow):
@@ -70,10 +89,11 @@ class MainWindow(QMainWindow):
         BSIZE = 80
         self.log_on = False
         self.log_file = None
+        self.sound_file = None
 
         # LINE DISPLAY ===================
-        init_line = line()
-        self.label = QLabel(init_line)
+        self.line = Line()
+        self.label = QLabel(self.line.display)
         lab_font = self.label.font()
         lab_font.setPixelSize(200)
         lab_font.setBold(True)
@@ -83,7 +103,7 @@ class MainWindow(QMainWindow):
         self.label.setAlignment(Qt.AlignCenter)
         layout.addWidget(self.label, alignment=Qt.AlignCenter)
 
-        # GENERATE & LOG BUTTONS ================================
+        # BUTTONS ================================
         btn_maingrp = QHBoxLayout()
         btn_maingrp.setAlignment(Qt.AlignHCenter)
         btn_maingrp.setSpacing(50)
@@ -97,6 +117,12 @@ class MainWindow(QMainWindow):
         self.btn_generate.setFixedSize(BSIZE, BSIZE)
         btn_maingrp.addWidget(self.btn_generate)
 
+        self.btn_play = QPushButton(u'\u25B6')
+        self.btn_play.clicked.connect(self.play)
+        self.btn_play.setFont(btn_font)
+        self.btn_play.setFixedSize(BSIZE, BSIZE)
+        btn_maingrp.addWidget(self.btn_play)
+
         self.btn_log = QPushButton(u'\u25CF')
         self.btn_log.setCheckable(True)
         self.btn_log.clicked.connect(self.toggle_log)
@@ -105,11 +131,57 @@ class MainWindow(QMainWindow):
         btn_maingrp.addWidget(self.btn_log)
 
     def generate(self):
-        this_line = line()
-        self.label.setText(this_line)
+        self.line = Line()
+        self.label.setText(self.line.display)
         if self.log_on:
             with open(self.log_file, 'a') as f:
-                f.write(this_line.replace('-', '') + '\n')
+                f.write(self.line.text + '\n')
+
+    def play(self):
+        text = self.line.text
+
+        langs = []
+        for lang in random.choices(LANGS, k=3):
+            if lang in list(ACCENT):
+                langs.append((lang, random.choice(ACCENT[lang])))
+            else:
+                langs.append((lang, 'com'))
+
+        slow = random.choice([True, False])
+
+        mp3_files = []
+        for i in range(len(langs)):
+            name = str(uuid.uuid4()) + '.mp3'
+            mp3_files.append(TMP/name)
+
+        for i in range(len(mp3_files)):
+            try:
+                tts = gtts.gTTS(
+                    text=text,
+                    lang=langs[i][0],
+                    tld=langs[i][1],
+                    slow=slow
+                )
+            except gtts.gTTSError:
+                tts = gtts.gTTS(
+                    text=text,
+                    lang=langs[i][0],
+                    tld=langs[i][1],
+                    slow=slow
+                )
+            tts.save(mp3_files[i])
+
+        audio = [AudioSegment.from_mp3(str(f)) for f in mp3_files]
+        audio.sort(key=lambda x: len(x), reverse=True)
+
+        mixed = audio[0]
+        for i in range(1, len(audio)):
+            mixed = mixed.overlay(audio[i])
+        mix_name = str(uuid.uuid4()) + '.mp3'
+        mixed.export(TMP/mix_name, format='wav')
+        self.sound_file = TMP/mix_name
+
+        playsound(self.sound_file)
 
     def toggle_log(self, checked):
         if not self.log_file:
