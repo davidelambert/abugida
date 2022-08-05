cb948c1cc412441c0a365e62a81afa84877f5c26
We Rock <werockemailaddress@gmail.com>
2022-08-04 21:00:48 -0400

    add letter selection GroupBoxes

--- abugida_5.py
+++ abugida_5.py
@@ -7,6 +7,7 @@ from typing import Union
 from pySpeakNG import speak as espeak
 from pySpeakNG import LANGUAGES, VOICES
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import *
 
 HERE = Path(__file__).parent.resolve()
@@ -18,7 +19,7 @@ CALV = {'t': 't', 'd': 'd', 's': 's', 'z': 'z', }
 CPALV = {'tʃ': 'tS', 'dʒ': 'dZ', 'ʃ': 'S', 'ʒ': 'Z', }
 CVEGL = {'k': 'k', 'g': 'g', 'x': 'x', 'h': 'h', }
 CNAS = {'m': 'm', 'n': 'n', 'ŋ': 'N', }
-CAPP = {'l': 'l', 'ɹ': 'r', 'j': 'j', 'w': 'w', }
+CAPP = {'l': 'l', 'r': 'r', 'j': 'j', 'w': 'w', }
 CON = CLAB | CDENT | CALV | CPALV | CVEGL | CNAS | CAPP
 
 VA = {'æ': '{', 'ɑ': 'A', 'ɑː': 'A:', 'ɒ': 'Q', }
@@ -128,3 +129,99 @@ class Line:
                       for i in range(n_words)]
         self.xsampa = ' '.join([w.xsampa for w in self.words])
         self.ipa = ' '.join([w.ipa for w in self.words])
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Abugida 5: Einstürzende Zikkuraten")
+        self.setWindowIcon(QIcon(str(HERE/'img/abugida_icon.svg')))
+        self.showMaximized()
+        layout = QVBoxLayout()
+        layout.setContentsMargins(50, 50, 50, 50)
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+        BW = 160  # Button Width
+        BH = 40   # Button Height
+        self.log_on = False
+        self.log_file = None
+
+        # LETTER SELECTION ================================
+        select = QHBoxLayout()
+        select.setContentsMargins(50, 0, 50, 0)
+        layout.addLayout(select)
+
+        self.vow_active = random.sample(list(VOW.values()), 3)
+        self.con_active = random.sample(list(CON.values()), 3)
+
+        # CONSONANTS =============================================
+        cgrid = QGridLayout()
+        cgrid.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+        cgrid.setContentsMargins(20, 20, 20, 20)
+        cgrid.setHorizontalSpacing(50)
+        cgrid.setVerticalSpacing(25)
+
+        self.con_cb = []
+        con_dicts = [CLAB, CDENT, CALV, CPALV, CVEGL, CNAS, CAPP]
+        for lset in con_dicts:
+            for ipa, xsampa in lset.items():
+                exec("self.cb_{} = QCheckBox('{}')".format(xsampa, ipa))
+                if xsampa in self.con_active:
+                    exec("self.cb_{}.setCheckState(Qt.Checked)".format(xsampa))
+                exec("self.cb_{}.stateChanged.connect(self.update_con)"
+                     .format(xsampa))
+                exec("cgrid.addWidget(self.cb_{}, {}, {})"
+                     .format(xsampa, con_dicts.index(lset),
+                             list(lset).index(ipa)))
+                exec("self.con_cb.append(self.cb_{})".format(xsampa))
+
+        cgroup = QGroupBox('Consonants')
+        cgroup.setLayout(cgrid)
+        cgroup.setFixedSize(400, 400)
+        select.addWidget(cgroup, alignment=Qt.AlignTop)
+
+        # VOWELS =============================================
+        vgrid = QGridLayout()
+        vgrid.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+        vgrid.setContentsMargins(20, 20, 20, 20)
+        vgrid.setHorizontalSpacing(50)
+        vgrid.setVerticalSpacing(25)
+
+        self.vow_cb = []
+        vow_dicts = [VA, VE, VI, VO, VU, VD1, VD2, ]
+        for lset in vow_dicts:
+            for ipa, xsampa in lset.items():
+                if xsampa == '{':
+                    xsampa = 'ae'
+                if xsampa.endswith(':'):
+                    xsampa = xsampa[0] + xsampa[0]
+                if '@' in xsampa:
+                    xsampa = xsampa.replace('@', 'uh')
+                exec("self.cb_{} = QCheckBox('{}')".format(xsampa, ipa))
+                if xsampa in self.vow_active:
+                    exec("self.cb_{}.setCheckState(Qt.Checked)".format(xsampa))
+                exec("self.cb_{}.stateChanged.connect(self.update_vow)"
+                     .format(xsampa))
+                exec("vgrid.addWidget(self.cb_{}, {}, {})"
+                     .format(xsampa, vow_dicts.index(lset),
+                             list(lset).index(ipa)))
+                exec("self.vow_cb.append(self.cb_{})".format(xsampa))
+
+        vgroup = QGroupBox('Vowels')
+        vgroup.setLayout(vgrid)
+        vgroup.setFixedSize(400, 400)
+        select.addWidget(vgroup, alignment=Qt.AlignTop)
+
+    def update_con(self):
+        pass
+
+    def update_vow(self):
+        pass
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec_()
